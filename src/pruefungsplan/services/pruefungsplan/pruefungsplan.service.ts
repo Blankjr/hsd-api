8da552d5de07b2cf8ec0477878dc16b17a058dbf
Christian <blankj.me@gmail.com>
2022-08-18 22:05:04 +0200

    Fix FrontEnd compatibility

--- src/pruefungsplan/services/pruefungsplan/pruefungsplan.service.ts
+++ src/pruefungsplan/services/pruefungsplan/pruefungsplan.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as csvToJson from 'convert-csv-to-json';
+import { Pruefung } from 'src/pruefungsplan/controllers/pruefungsplan/pruefungsplan.controller';
 @Injectable()
 export class PruefungsplanService {
   async loadData(jsonLocation: string, csvLocation: string) {
@@ -23,7 +24,16 @@ export class PruefungsplanService {
   }
 
   private baseParse(jsonLocation: string) {
-    const jsonObject = fs.readFileSync(jsonLocation);
-    return jsonObject.toString();
+    const jsonObject = fs.readFileSync(jsonLocation)
+    let exams: Array<Pruefung> = JSON.parse(jsonObject.toString())
+    exams = exams.map(element => {
+      return this.convertForFrontEnd(element)
+    });
+
+    return JSON.stringify(exams);
+  }
+  private convertForFrontEnd(exam: Pruefung) {
+    exam.Datum = exam.Datum.replace(/\//g, '.')
+    return exam
   }
 }
